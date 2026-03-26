7ce5803c2b8750f933a0a8d296a91fca5a9967a2
Abgar Uyar <abgar-uyar@web.de>
2026-03-26 16:07:29 +0100

    Rechener

--- test/MainWindow.xaml.cs
+++ test/MainWindow.xaml.cs
@@ -19,6 +19,87 @@ namespace test
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HandleOperator(e);
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            string ClalcName = "Rechner";
+            meintext.Text = ClalcName;
+            result.Text = "Ergebnis";
+            numb1.Text = "";
+            numb2.Text = "";
+
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter) 
+            {
+                HandleEnter(e);
+            }
+            else if (e.Key == Key.Escape) 
+            {
+                Button_Clear(sender, e);
+            }
+            else if (e.Key == Key.Add || e.Key == Key.Subtract || e.Key == Key.Multiply || e.Key == Key.Divide) 
+            {
+                HandleOperator(e);
+            }
+        }
+        private void HandleEnter(KeyEventArgs e)
+        {
+            if (numb1.Text.Length == 0)
+            {
+                numb1.Focus();
+            }
+            else if (numb2.Text.Length == 0)
+            {
+                numb2.Focus();
+            }
+            else if (numb1.Text.Length > 0 && numb2.Text.Length > 0)
+            {
+                HandleOperator(e);
+            }
+        }
+        private void HandleOperator(KeyEventArgs e)
+        {
+            int number1;
+            int number2;
+            if (int.TryParse(numb1.Text, out number1) && int.TryParse(numb2.Text, out number2))
+            {
+                if (operatorBox.SelectedItem == null)
+                {   
+                    meintext.Text = "Bitte einen Operator auswählen";
+                    return;
+                }
+                ComboBoxItem selectedItem = (ComboBoxItem)operatorBox.SelectedItem;
+                string op = selectedItem.Content.ToString();
+
+                int ergebnis = 0;
+
+                if (e.Key == Key.Add || op == "+")
+                {
+                    ergebnis = number1 + number2 ;
+                }
+                else if (e.Key == Key.Subtract || op == "-")
+                {
+                    ergebnis = number1 - number2;
+                }
+                else if (e.Key == Key.Multiply || op == "*")
+                {
+                    ergebnis = number1 * number2;
+                }
+                else if (e.Key == Key.Divide || op == "/")
+                {
+                    ergebnis = number1 / number2;
+                }
+                result.Text = ergebnis.ToString();
+            }
+        }
+
     }
 }
